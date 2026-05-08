f95ca581001a43b36f349b43c83510215cb158e1
Gangothri-N <gangothrin.22cse@cambridge.edu.in>
2026-05-08 17:51:35 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -7,14 +7,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("House Price Prediction")
 
-square_feet = st.number_input("Square Feet")
-bedrooms = st.number_input("Bedrooms")
-bathrooms = st.number_input("Bathrooms")
+# Inputs
+square_footage = st.number_input("Square Footage")
+num_bedrooms = st.number_input("Number of Bedrooms")
+num_bathrooms = st.number_input("Number of Bathrooms")
 year_built = st.number_input("Year Built")
+lot_size = st.number_input("Lot Size")
+garage_size = st.number_input("Garage Size")
+neighborhood_quality = st.number_input("Neighborhood Quality")
 
+# Prediction
 if st.button("Predict Price"):
 
-    features = np.array([[square_feet, bedrooms, bathrooms, year_built]])
+    features = np.array([[square_footage,
+                          num_bedrooms,
+                          num_bathrooms,
+                          year_built,
+                          lot_size,
+                          garage_size,
+                          neighborhood_quality]])
 
     prediction = model.predict(features)
 
